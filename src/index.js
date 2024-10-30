c3aa94cf13590bb0aae491a922558d8383fdded2
Vedagya <vedagyajha22@outlook.com>
2024-10-30 12:27:53 +0000

    fixed paths not working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
+
 const middleware = require("./utils/middleware");
 const loginRouter = require("./controllers/login");
 const signUpRouter = require("./controllers/signup");
@@ -20,7 +22,7 @@ mongoose
 	})
 	.catch((error) => console.log("error connecting to MongoDB"));
 
-app.use(express.static("dist"));
+app.use(express.static(path.resolve(__dirname, "../dist")));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +30,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/signup", signUpRouter);
 app.use("/login", loginRouter);
 app.use("/passwords", passwordRouter);
+
+// Catch-all route to serve index.html for React Router
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
